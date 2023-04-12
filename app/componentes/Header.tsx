e590b958b8683f68250fe0c501eda4f03569e6c1
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-04-12 18:35:47 +0800

    Header for big screens only

--- app/componentes/Header.tsx
+++ app/componentes/Header.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import Image from "next/image";
 
 function Header() {
-  return <div className="text-4xl bg-red-500">Headeasdasdasdr</div>;
+  return (
+    <div className="flex flex-row justify-between items-center my-4 mx-12">
+      <div className="flex flex-row gap-16 items-center">
+        <Image
+          src="/logo/logo.svg"
+          width={240}
+          height={40}
+          alt="logo"
+          className=""
+        />
+        <ul className="flex flex-row gap-12 h-fit text-[#B20000] font-semibold">
+          <li>Jobs</li>
+          <li>Articles</li>
+          <li>Companies</li>
+          <li>Support</li>
+        </ul>
+      </div>
+      <a
+        href="#"
+        className=" border border-red-600 py-1 px-12 mr-6 rounded-full"
+      >
+        Sign In
+      </a>
+    </div>
+  );
 }
 
 export default Header;
