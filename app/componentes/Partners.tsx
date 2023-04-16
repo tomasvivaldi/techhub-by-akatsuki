3047abd678ec633dd9d9fdd4bd7a9e5d53a0424f
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-04-16 12:27:37 +0800

    Partners section

--- app/componentes/Partners.tsx
+++ app/componentes/Partners.tsx
@@ -1,7 +1,69 @@
 import React from "react";
+import Image from "next/image";
 
 function Partners() {
-  return <div>Partners</div>;
+  return (
+    <div className="flex flex-col my-16">
+      <h3 className="text-3xl w-full text-center font-light my-4">
+        Our Partners
+      </h3>
+      <hr className="w-[50px] border-black mx-auto my-8" />
+      <div className="flex flex-col mx-16">
+        <div className="flex flex-row gap-8  ml-20 px-auto">
+          <Image
+            src="logo/WeTech.svg"
+            width={260}
+            height={260}
+            alt="partner logo"
+          />
+          <Image
+            src="logo/Caica.svg"
+            width={260}
+            height={260}
+            alt="partner logo"
+          />
+          <Image
+            src="logo/Beenos.svg"
+            width={260}
+            height={260}
+            alt="partner logo"
+          />
+          <Image
+            src="logo/Shift.svg"
+            width={260}
+            height={260}
+            alt="partner logo"
+          />
+        </div>
+        <div className="flex flex-row gap-8  ml-36 px-auto">
+          <Image
+            src="logo/Wamazing.svg"
+            width={260}
+            height={260}
+            alt="partner logo"
+          />
+          <Image
+            src="logo/TeamLab.svg"
+            width={260}
+            height={260}
+            alt="partner logo"
+          />
+          <Image
+            src="logo/BitFlyer.svg"
+            width={260}
+            height={260}
+            alt="partner logo"
+          />
+          <Image
+            src="logo/YahooJapan.svg"
+            width={260}
+            height={260}
+            alt="partner logo"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Partners;
